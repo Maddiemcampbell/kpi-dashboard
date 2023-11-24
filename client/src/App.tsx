ecefa43399136e850b25fb536a6a5676c1888918
Maddie <maddie@Maddies-Air.attlocal.net>
2023-11-24 00:19:09 -0800

    style: mui theme and set css to baseline

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,15 @@
+import { useMemo } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { themeSettings } from "./theme";
+
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
     </div>
   )
 }
