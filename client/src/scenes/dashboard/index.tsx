16a846d1ef5cea56ff17834aecd86eeefdd8192a
Maddie <maddie@Maddies-Air.attlocal.net>
2023-11-24 23:22:22 -0800

    why are you looking at my commits?: cleaning her up

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,7 +1,7 @@
-import DashboardBox from "@/components/DashboardBox";
 import { Box, useMediaQuery, useTheme}  from "@mui/material";
-
-type Props = {}
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
     " a b c"
@@ -49,9 +49,8 @@ const gridTemplateSmallScreens = `
   "j"
 `;
 
-const Dashboard = (props: Props) => {
+const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
-  const { palette } = useTheme();
   return (
     <Box width="100%" height="100%" display="grid" gap="1.5rem"
         sx={
@@ -68,16 +67,9 @@ const Dashboard = (props: Props) => {
                 }
         }
     >
-        <DashboardBox gridArea="a"></DashboardBox>
-        <DashboardBox gridArea="b"></DashboardBox>
-        <DashboardBox gridArea="c"></DashboardBox>
-        <DashboardBox gridArea="d"></DashboardBox>
-        <DashboardBox gridArea="e"></DashboardBox>
-        <DashboardBox gridArea="f"></DashboardBox>
-        <DashboardBox gridArea="g"></DashboardBox>
-        <DashboardBox gridArea="h"></DashboardBox>
-        <DashboardBox gridArea="i"></DashboardBox>
-        <DashboardBox gridArea="j"></DashboardBox>
+        <Row1 />
+        <Row2 />
+        <Row3 />
     </Box>
   );
 };
