3a2e467c3c678a7fbd227a598cd50048a710f207
Maddie <maddie@Maddies-Air.attlocal.net>
2023-11-26 23:58:41 -0800

    build: day and month schemas for kpis

--- server/models/KPI.js
+++ server/models/KPI.js
@@ -2,6 +2,52 @@ import mongoose from "mongoose";
 import {loadType} from "mongoose-currency"
 
 const Schema = mongoose.Schema;
+loadType(mongoose);
+
+const daySchema = new Schema(
+    {
+      date: String,
+      revenue: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100,
+      },
+      expenses: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100,
+      },
+    },
+    { toJSON: { getters: true } }
+  );
+  
+
+const monthSchema = new Schema(
+    {
+      month: String,
+      revenue: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100,
+      },
+      expenses: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100,
+      },
+      operationalExpenses: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100,
+      },
+      nonOperationalExpenses: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => v / 100,
+      },
+    },
+    { toJSON: { getters: true } }
+  );
 
 const KPISchema = new Schema({
     totalProfit: {
